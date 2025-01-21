6f2fa4fe44efede1eb1c427c62d78b3433c9d3f1
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-01-21 20:09:32 +0200

    add utils and generate funct ions

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+import { createFakeContact } from '../utils/createFakeContact';
+
+const generateContacts = async (number) => {
+  try {
+    const newContact = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+    const updatedContacts = [...readContacts, ...newContact];
+    await writeContacts(updatedContacts);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
+import fs from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts);
+    await fs.writeFile(PATH_DB, data, 'utf8');
+    console.log('The data was successfully written to the file');
+  } catch (err) {
+    console.error('Error writing to file:', err);
+  }
+};
